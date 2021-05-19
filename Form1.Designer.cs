99b69655b11d8d4527ebd558381d52a6668a8bd2
ahmmedabdelalim <am909962@gmail.com>
2021-05-19 14:22:24 +0200

    DAA & Bresen

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace line_algo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_X1 = new System.Windows.Forms.TextBox();
+            this.text_Y1 = new System.Windows.Forms.TextBox();
+            this.text_X2 = new System.Windows.Forms.TextBox();
+            this.text_Y2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(36, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(235, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(446, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "X2";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(648, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Y2";
+            // 
+            // text_X1
+            // 
+            this.text_X1.Location = new System.Drawing.Point(77, 21);
+            this.text_X1.Name = "text_X1";
+            this.text_X1.Size = new System.Drawing.Size(100, 23);
+            this.text_X1.TabIndex = 5;
+            this.text_X1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // text_Y1
+            // 
+            this.text_Y1.Location = new System.Drawing.Point(275, 21);
+            this.text_Y1.Name = "text_Y1";
+            this.text_Y1.Size = new System.Drawing.Size(100, 23);
+            this.text_Y1.TabIndex = 6;
+            // 
+            // text_X2
+            // 
+            this.text_X2.Location = new System.Drawing.Point(487, 21);
+            this.text_X2.Name = "text_X2";
+            this.text_X2.Size = new System.Drawing.Size(100, 23);
+            this.text_X2.TabIndex = 7;
+            // 
+            // text_Y2
+            // 
+            this.text_Y2.Location = new System.Drawing.Point(688, 21);
+            this.text_Y2.Name = "text_Y2";
+            this.text_Y2.Size = new System.Drawing.Size(100, 23);
+            this.text_Y2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(49, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 28);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "DDA";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(444, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Bresenham";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Draw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // picture
+            // 
+            this.picture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picture.Location = new System.Drawing.Point(36, 152);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(339, 356);
+            this.picture.TabIndex = 17;
+            this.picture.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picture2.Location = new System.Drawing.Point(444, 152);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(339, 356);
+            this.picture2.TabIndex = 18;
+            this.picture2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(688, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 567);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.text_Y2);
+            this.Controls.Add(this.text_X2);
+            this.Controls.Add(this.text_Y1);
+            this.Controls.Add(this.text_X1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_X1;
+        private System.Windows.Forms.TextBox text_Y1;
+        private System.Windows.Forms.TextBox text_X2;
+        private System.Windows.Forms.TextBox text_Y2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace line_algo
+{
+    public partial class Form1 : Form
+    {
+        public void ddaline(Point p1, Point p2)
+        {
+            int k ;
+            double xinc, yinc, x, x1, y, y1, steps;
+
+            Bitmap p = new Bitmap(this.Width, this.Height);
+            double deltaX = p2.X - p1.X;
+            double deltaY = p2.Y - p1.Y;
+            if (deltaX > deltaY) steps = deltaX;
+            else steps = deltaY;
+            xinc = deltaX / steps;
+            yinc = deltaY / steps;
+            x = x1 = p1.X;
+            y = y1 = p1.Y;
+
+            //casting
+
+            p.SetPixel((int)x, (int)y, Color.Red);
+
+
+            // k1 steps
+            
+            for (k = 1; k <= steps; k++)
+            {
+                x = x + xinc;
+                x = Math.Round(x, 0);
+
+                y = y + yinc;
+                y = Math.Round(y, 0);
+
+                p.SetPixel((int)x, (int)y, Color.Red );
+                 
+
+
+            }
+            
+            picture.Image = p;
+
+        }
+        /// <summary>
+         
+        public void bresenham (Point p1 , Point p2 )
+        {
+            int deltaX = p2.X - p1.X;
+            int deltaY = p2.Y - p1.X;
+            int x, y;
+            Bitmap po = new Bitmap(this.Width, this.Height);
+            int p = 2 * deltaY - deltaX;
+            int twoDy = 2 * deltaY;
+            int twoDyMinusDx = 2 * (deltaY - deltaX);
+
+            if (p1.X > p2.X)
+            {
+                x = p2.X;
+                y = p2.Y;
+                p2.X = p1.X;
+            }
+            else { x = p1.X;
+                y = p1.Y;
+            }
+
+            po.SetPixel((int)x, (int)y, Color.Red);
+
+            while(x < p2.X)
+            {
+                x++;
+                if (p < 0)
+                    p += twoDy;
+                else
+                {
+                    y++;
+                    p += twoDyMinusDx;
+
+                }
+
+                po.SetPixel((int)x, (int)y, Color.Green);
+
+            }
+
+            picture2.Image = po;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(text_X1.Text);
+            int y1 = Convert.ToInt32(text_Y1.Text);
+
+            int x2 = Convert.ToInt32(text_X2.Text);
+            int y2 = Convert.ToInt32(text_Y2.Text);
+
+
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+
+            // call DDA_fun
+
+            //ddaline(p1,p2);
+            ddaline(p1, p2);
+
+
+        }
+
+
+
+        
+
+        private void panel_DDA_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(text_X1.Text);
+            int y1 = Convert.ToInt32(text_Y1.Text);
+
+            int x2 = Convert.ToInt32(text_X2.Text);
+            int y2 = Convert.ToInt32(text_Y2.Text);
+
+
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+
+            // call DDA_fun
+
+            //ddaline(p1,p2);
+            bresenham(p1, p2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
